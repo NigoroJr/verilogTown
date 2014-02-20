45b691617214b596e34b728c26c7172530c699fd
collinsjm <collin15@miamioh.edu>
2014-02-20 12:43:18 -0500

    Added slightly modified LevelScreen and single car sprite
    LibGDX has some issues with images that aren't exact powers of 2
    apparently, so for the time being I'm just using a single frame copied
    to a new image for the car. The movement via arrow keys code is
    commented out, in case it is unnecessary.

--- VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/LevelScreen.java
+++ VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/LevelScreen.java
@@ -1,22 +1,31 @@
 package com.me.myverilogTown;
 
 import java.util.*;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-	 
+import com.badlogic.gdx.math.Vector2;
+import com.me.myverilogTown.Car;
+
 public class LevelScreen implements Screen 
 {
 	final verilogTown game;
-	 
+
 	private OrthographicCamera camera;
 	private Texture level_map;
 	private SpriteBatch thebatch;
 	private LevelLogic levelLogic;
-	 
+	private Car testCar;
+	private Sprite carSprite;
+	private Texture tmp;
+
 	public LevelScreen(final verilogTown gam) 
 	{
 		/* initialize the level logic control */
@@ -26,59 +35,101 @@ public class LevelScreen implements Screen
 
 		thebatch = new SpriteBatch();
 		/* initialize the map */
-		level_map = new Texture(Gdx.files.internal("data/first_map.png"));
-	 
-		// create the camera and the SpriteBatch
+		level_map = new Texture("asset_resources/tiled_maps/first_map.png");
+
+		// create the camera for the SpriteBatch
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 1280, 1280);
+
+		tmp = new Texture("asset_resources/cars/CAR_BLUE_WHITE_STRIPE_SINGLE.png");
+
+		tmp.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		testCar = new Car(new Vector2(640,640), 64, 64, 0, 100f, tmp);
+		Gdx.input.setInputProcessor(new InputHandler(this));
+
+
 	}
-	 
+
 	@Override
 	public void render(float delta) 
 	{
 		levelLogic.update();
+
+		/*
+		 * Below is the test code for redrawing, rotating, and moving the sprite about the map
+		 * 
+		if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) {
+			testCar.getCarSprite().setRotation(-180);
+			testCar.getCarSprite().setX(testCar.getCarSprite().getX()-(Gdx.graphics.getDeltaTime() * testCar.speed));
+		}
+		if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) {
+			testCar.getCarSprite().setRotation(0);
+			testCar.getCarSprite().setX(testCar.getCarSprite().getX() + (Gdx.graphics.getDeltaTime() * testCar.speed));
+		}
 		
+		//For some reason, north/south movement is slower than east/west. Probably due to widescreen screen resolutions. Multiplying
+		//Velocity by 1.5 seems to make it look at least similar, speed wise - that's why it's cast as a float. 
+
+		if(Gdx.input.isKeyPressed(Keys.DPAD_UP)) {
+			testCar.getCarSprite().setRotation(90);
+			testCar.getCarSprite().setY((float) (testCar.getCarSprite().getY() + (Gdx.graphics.getDeltaTime() * testCar.speed * 1.5)));
+		}
+		if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) {
+			testCar.getCarSprite().setRotation(-90);
+			testCar.getCarSprite().setY((float) (testCar.getCarSprite().getY() - (Gdx.graphics.getDeltaTime() * testCar.speed * 1.5)));
+		}
+	
+		*/
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-	 
+
 		// tell the camera to update its matrices.
 		camera.update();
 
 		thebatch.begin();
 		thebatch.draw(level_map, 0, 0);
+		testCar.getCarSprite().draw(thebatch);
 		thebatch.end();
+
 	}
-	 
+
 	@Override
 	public void resize(int width, int height) 
 	{
 	}
-	 
+
 	@Override
 	public void show() 
 	{
 	}
-	 
+
 	@Override
 	public void hide() 
 	{
 	}
-	 
+
 	@Override
 	public void pause() 
 	{
 	}
-	 
+
 	@Override
 	public void resume() 
 	{
 	}
-	 
+
 	@Override
 	public void dispose() 
 	{
-	
+
 	}
-	 
+
+	public Car getCar() {
+		return testCar;
+		// TODO Auto-generated method stub
+	}
 	
+
+
 }
