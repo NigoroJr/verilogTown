c4468365451fc0d7a06756e8479d149d6806006d
Naoki Mizuno <nm2372@gmail.com>
2014-03-28 19:05:53 -0400

    Fix explanation figures of grids

--- VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/GridType.java
+++ VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/GridType.java
@@ -20,49 +20,53 @@ public enum GridType
 
     /*
      * corner 1
-     * ____
+     * +-----
      * |a
-     * | b_
-     * | |
+     * | b
+     * |  +--
+     * |  |
      */
     CORNER_ROAD_W2S, // a West then South
     CORNER_ROAD_N2E, // b
     /*
      * corner 2
-     * ____
-     * b|
-     * _a |
-     * | |
+     * ----+
+     *    b|
+     *   a |
+     * -+  |
+     *  |  |
      */
     CORNER_ROAD_E2S, // a
     CORNER_ROAD_N2W, // b
     /*
      * corner 3
-     * | |
-     * - |
-     * a |
-     * b|
-     * ----
+     *  |  |
+     * -+  |
+     *   a |
+     *    b|
+     * ----+
      */
     CORNER_ROAD_S2W, // a
     CORNER_ROAD_E2N, // b
     /*
      * corner 4
-     * | |
-     * | -
+     * |  |
+     * |  +-
      * | b
      * |a
-     * ----
+     * +----
      */
     CORNER_ROAD_S2E, // a
     CORNER_ROAD_W2N, // b
 
     /*
      * Full Intersection
-     * !!
-     * -ab-
-     * -cd-
-     * ||
+     *   |  |
+     * --+  +--
+     *    ab
+     *    cd
+     * --+  +--
+     *   |  |
      */
     INTER_TURN_N2EN, // d links to North and East
     INTER_TURN_W2NW, // b
@@ -71,11 +75,13 @@ public enum GridType
     /*
      * Sample Partial Intersection - Note this is doable since all nodes only
      * point forward. If backwards have more cases
-     * -ab-
-     * -cd-
-     * ||
+     * ----------
+     *     ab
+     *     cd
+     * ---+  +---
+     *    |  |
      * b = STRAIGHT_ROAD_W
-     * a = INTER_TURN_S2WS // special case since no in path, but doesn;t matter
+     * a = INTER_TURN_S2WS // special case since no in path, but doesn't matter
      * c = INTER_TURN_E2SE
      * d = INTER_TURN_N2EN
      */
