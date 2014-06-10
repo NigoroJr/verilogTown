NigoroJr/verilogTown

commit 534c2978740ae338c342f025417bb1e5ff6f9446

- Added License MIT - Added Single click sim in Editor - Added Clk count to Editor - Added Reset "R" Button to VerilogTown

VerilogEditor/src/VerilogEditor.java
@@ -1,3 +1,29 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Peter Jamieson, Naoki Mizuno, and Boyu Zhang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
+package src;
+
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -493,10 +519,17 @@ public class VerilogEditor extends JFrame implements ActionListener
 			out.close();
 			
 			/* print out what we're compiling */
-			errorText.setText("Compiling"+path + "VerilogFiles/" + name + ".txt");
+			errorText.setText("Compiling "+path + "VerilogFiles/" + name + ".txt");
 
 			/* parse the base file */
 			Compiler.compileFileForEditor(path + "VerilogFiles/" + name + ".txt");
+
+			if (Compiler.is_compiled_yet())
+			{
+				/* Reset the system - takes a double simulation */
+				Compiler.sim_cycle("0", "00000000", "000000000000000000000000000000");
+				Compiler.sim_cycle("1", "00000000", "000000000000000000000000000000");
+			}
 		}
 		catch (Exception e1)
 		{
@@ -659,9 +692,14 @@ public class VerilogEditor extends JFrame implements ActionListener
 
 			if (Compiler.is_compiled_yet())
 			{
-				ArrayList<Integer> output_vector_list = Compiler.sim_cycle(simulateBin.substring(0,1), simulateBin.substring(1, 9), simulateBin.substring(9, 40));
+				ArrayList<Integer> output_vector_list;
+
+				/* first sim is for the clock cycle */
+				output_vector_list = Compiler.sim_cycle("1", simulateBin.substring(1, 9), simulateBin.substring(9, 40));
+				/* first sim is for the combinational propagation */
+				output_vector_list = Compiler.sim_cycle("1", simulateBin.substring(1, 9), simulateBin.substring(9, 40));
 			
-				errorText.setText("Simulation Cycle\nReset is: " + simulateBin.substring(0,1) + " Sensors Light: " + simulateBin.substring(1, 9)+" General Sensors: " + simulateBin.substring(9, 40)+"\nOut0 Val:"+output_vector_list.get(0) + "\nOut1 Val:"+output_vector_list.get(1) +"\nOut:2 Val:"+output_vector_list.get(2) +"\nOut3 Val:"+output_vector_list.get(3)+"\nDebugVector:"+Integer.toBinaryString(output_vector_list.get(4)));
+				errorText.setText("Simulation Cycle\nReset is: " + simulateBin.substring(0,1) + "\nClock Cycle:"+output_vector_list.get(5)+ " Sensors Light: " + simulateBin.substring(1, 9)+" General Sensors: " + simulateBin.substring(9, 40)+"\nOut0 Val:"+output_vector_list.get(0) + "\nOut1 Val:"+output_vector_list.get(1) +"\nOut:2 Val:"+output_vector_list.get(2) +"\nOut3 Val:"+output_vector_list.get(3)+"\nDebugVector:"+Integer.toBinaryString(output_vector_list.get(4)));
 			}
 			else
 			{

VERILOG_TOWN/verilogTown-desktop/src/com/me/myverilogTown/Main.java
@@ -1,16 +1,42 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Peter Jamieson, Naoki Mizuno, and Boyu Zhang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
 package com.me.myverilogTown;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class Main {
-    public static void main(String[] args) {
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "verilogTown";
-        cfg.useGL20 = false;
-        cfg.width = 600;
-        cfg.height = 600;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "verilogTown";
+		cfg.useGL20 = false;
+		cfg.width = 600;
+		cfg.height = 600;
 
-        new LwjglApplication(new verilogTown(), cfg);
-    }
+		new LwjglApplication(new verilogTown(), cfg);
+	}
 }

VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/AudioLoader.java
@@ -1,39 +1,0 @@
-package com.me.myverilogTown;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Sound;
-
-/*
- * Class used to simplify loading audio files for VerilogTown
- */
-
-public class AudioLoader {
-	
-	private Sound collision;
-
-/*
- * This constructor used to specify a new collision file
- */
-	public AudioLoader(Sound collision){
-		this.collision = collision;
-	}
-	
-/*
- * This constructor used to use the file I'm currently using
- */
-	public AudioLoader(){
-		this.collision = Gdx.audio.newSound(Gdx.files.internal("myleg.mp3"));
-	}
-
-	public Sound getCollisionSound() {
-		return collision;
-	}
-
-	public void setCollisionSound(Sound collision) {
-		this.collision = collision;
-	}
-	
-
-	
-
-}

VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/Direction.java
@@ -1,9 +1,30 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Peter Jamieson, Naoki Mizuno, and Boyu Zhang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
 package com.me.myverilogTown;
 
 public enum Direction
 {
-	N,
-	S,
-	E,
-	W
+	N, S, E, W
 }

VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/EffectsLoader.java
@@ -1,27 +1,0 @@
-package com.me.myverilogTown;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.ParticleEffect;
-
-/*
- * Class used to simplify loading of particle effects for collisions
- * 
- */
-public class EffectsLoader {
-	
-	private ParticleEffect fx;
-	
-	public EffectsLoader(){
-		fx = new ParticleEffect();
-		fx.load(Gdx.files.internal("ASSET_RESOURCES/PARTICLES/FLARE.p"), Gdx.files.internal("ASSET_RESOURCES"));
-	}
-
-	public ParticleEffect getFx() {
-		return fx;
-	}
-
-	public void setFx(ParticleEffect fx) {
-		this.fx = fx;
-	}	
-
-}

VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/GridNode.java
@@ -1,181 +1,190 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Peter Jamieson, Naoki Mizuno, and Boyu Zhang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
 package com.me.myverilogTown;
 
-public class GridNode 
+public class GridNode
 {
 	/* Bottom-left is (x, y) = (0, 0) */
-	private int x;
-	private int y;
+	private int				x;
+	private int				y;
 
-	private GridType grid_type;
-	private GridNode north;
-	private GridNode south;
-	private GridNode east;
-	private GridNode west;
+	private GridType		grid_type;
+	private GridNode		north;
+	private GridNode		south;
+	private GridNode		east;
+	private GridNode		west;
 
-	public TrafficControl signal = null;
-	/**
-	 * Location of the traffic light with respect to the intersection.
-	 * Read this as "traffic light on the {location} side of the intersection."
-	 */
-	private int location = -1;
+	public TrafficControl	signal			= null;
+	/** Location of the traffic light with respect to the intersection. Read this
+	 * as "traffic light on the {location} side of the intersection." */
+	private int				location		= -1;
 
-	private Car car = null;
+	private Car				car				= null;
 
 	/* Mark for BFS. Always greater than 0 */
-	private int visited_mark = 0;
+	private int				visited_mark	= 0;
 	/* Node that visited this node. Used for reverse traversal */
-	private GridNode visited_by = null;
+	private GridNode		visited_by		= null;
 
 	/* Constructor */
-	public GridNode(int x, int y, GridType type) 
+	public GridNode(int x, int y, GridType type)
 	{
 		this.x = x;
 		this.y = y;
 		this.grid_type = type;
 	}
 
-	public int getX() 
+	public void softReset()
+	{
+		car = null;
+	}
+
+	public int getX()
 	{
 		return this.x;
 	}
 
-	public int getY() 
+	public int getY()
 	{
 		return this.y;
 	}
 
-	public GridType getType() 
+	public GridType getType()
 	{
 		return grid_type;
 	}
 
-	public int getVisitedCount() 
+	public int getVisitedCount()
 	{
 		return this.visited_mark;
 	}
 
-	public void setVisited(int mark, GridNode by) 
+	public void setVisited(int mark, GridNode by)
 	{
 		this.visited_mark = mark;
 		this.visited_by = by;
 	}
 
-	public GridNode getVisitedBy() 
+	public GridNode getVisitedBy()
 	{
 		return this.visited_by;
 	}
 
-	public boolean isAlreadyVisited(int markPathCount) 
+	public boolean isAlreadyVisited(int markPathCount)
 	{
 		return markPathCount == this.visited_mark;
 	}
 
-	public void setNorth(GridNode dest) 
+	public void setNorth(GridNode dest)
 	{
 		this.north = dest;
 	}
 
-	public void setSouth(GridNode dest) 
+	public void setSouth(GridNode dest)
 	{
 		this.south = dest;
 	}
 
-	public void setWest(GridNode dest) 
+	public void setWest(GridNode dest)
 	{
 		this.west = dest;
 	}
 
-	public void setEast(GridNode dest) 
+	public void setEast(GridNode dest)
 	{
 		this.east = dest;
 	}
 
-	/**
-	 * Returns the grid north of this grid.
+	/** Returns the grid north of this grid.
 	 * 
-	 * @return The grid in the north.
-	 */
-	public GridNode getNorth() 
+	 * @return The grid in the north. */
+	public GridNode getNorth()
 	{
 		return this.north;
 	}
 
-	/**
-	 * Returns the grid south of this grid.
+	/** Returns the grid south of this grid.
 	 * 
-	 * @return The grid in the south.
-	 */
-	public GridNode getSouth() 
+	 * @return The grid in the south. */
+	public GridNode getSouth()
 	{
 		return this.south;
 	}
 
-	/**
-	 * Returns the grid east of this grid.
+	/** Returns the grid east of this grid.
 	 * 
-	 * @return The grid in the east.
-	 */
-	public GridNode getEast() 
+	 * @return The grid in the east. */
+	public GridNode getEast()
 	{
 		return this.east;
 	}
 
-	/**
-	 * Returns the grid west of this grid.
+	/** Returns the grid west of this grid.
 	 * 
-	 * @return The grid in the west.
-	 */
-	public GridNode getWest() 
+	 * @return The grid in the west. */
+	public GridNode getWest()
 	{
 		return this.west;
 	}
 
-	/**
-	 * Returns whether this grid is a traffic signal or not.
+	/** Returns whether this grid is a traffic signal or not.
 	 * 
-	 * @return True if this grid is a traffic signal, false if not.
-	 */
-	public boolean isTrafficSignal() 
+	 * @return True if this grid is a traffic signal, false if not. */
+	public boolean isTrafficSignal()
 	{
 		return signal != null;
 	}
 
-	/**
-	 * Sets a car on this grid.
+	/** Sets a car on this grid.
 	 * 
 	 * @param car
-	 *			The car that is currently on this grid.
-	 */
-	public void setCar(Car car) 
+	 *            The car that is currently on this grid. */
+	public void setCar(Car car)
 	{
 		this.car = car;
 	}
 
-	/**
-	 * Returns the car that is currently on the grid.
+	/** Returns the car that is currently on the grid.
 	 * 
-	 * @return The car that is currently on this grid.
-	 */
-	public Car getCar() 
+	 * @return The car that is currently on this grid. */
+	public Car getCar()
 	{
 		return car;
 	}
 
-	/**
-	 * Removes the car from this grid if there is one.
-	 */
-	public void removeCar() 
+	/** Removes the car from this grid if there is one. */
+	public void removeCar()
 	{
 		setCar(null);
 	}
 
-	/**
-	 * Returns the traffic signal on this grid. NO_SIGNAL is returned if there
+	/** Returns the traffic signal on this grid. NO_SIGNAL is returned if there
 	 * is no signal on this grid.
 	 * 
-	 * @return The status of the traffic signal on this grid.
-	 */
-	public TrafficSignalState getTrafficSignal() 
+	 * @return The status of the traffic signal on this grid. */
+	public TrafficSignalState getTrafficSignal()
 	{
 		// TODO: getSignalOn(location)
 		// return signal == null ? TrafficSignal.NO_SIGNAL :
@@ -186,24 +195,22 @@ public class GridNode
 			return TrafficSignalState.NO_SIGNAL;
 	}
 
-	public TrafficControl getTrafficControl() 
+	public TrafficControl getTrafficControl()
 	{
 		return signal;
 	}
 
-	/**
-	 * Relates this grid to the given traffic light. The <code>location</code>
+	/** Relates this grid to the given traffic light. The <code>location</code>
 	 * variable is given to inform of the relative location of this grid with
 	 * respect to the intersection.
 	 * 
 	 * @param traffic_signal
-	 *			The traffic light that this grid relates to.
+	 *            The traffic light that this grid relates to.
 	 * @param location
-	 *			The location of this grid with respect to the traffic light.
-	 *			Read this as "this grid is on the {location} side of
-	 *			{traffic_signal}".
-	 */
-	public void setTrafficControl(TrafficControl traffic_signal, int location) 
+	 *            The location of this grid with respect to the traffic light.
+	 *            Read this as "this grid is on the {location} side of
+	 *            {traffic_signal}". */
+	public void setTrafficControl(TrafficControl traffic_signal, int location)
 	{
 		this.signal = traffic_signal;
 		this.location = location;

VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/GridType.java
@@ -1,91 +1,70 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Peter Jamieson, Naoki Mizuno, and Boyu Zhang
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
+
 package com.me.myverilogTown;
 
 public enum GridType
 {
-    START_NEDGE2S, // means a start point on the north boarder so going south to
-                   // STRAIGHT_ROAD_S
-    START_SEDGE2N,
-    START_EEDGE2W,
-    START_WEDGE2E,
+	START_NEDGE2S, // means a start point on the north boarder so going south to
+					// STRAIGHT_ROAD_S
+	START_SEDGE2N,
+	START_EEDGE2W,
+	START_WEDGE2E,
 
-    END_S2SEDGE,
-    END_N2NEDGE,
-    END_W2WEDGE,
-    END_E2EEDGE,
+	END_S2SEDGE,
+	END_N2NEDGE,
+	END_W2WEDGE,
+	END_E2EEDGE,
 
-    // Read STRAIGHT_ROAD_W2W as "enter facing west, exit facing went"
-    STRAIGHT_ROAD_N2N,
-    STRAIGHT_ROAD_S2S,
-    STRAIGHT_ROAD_E2E,
-    STRAIGHT_ROAD_W2W,
+	// Read STRAIGHT_ROAD_W2W as "enter facing west, exit facing went"
+	STRAIGHT_ROAD_N2N,
+	STRAIGHT_ROAD_S2S,
+	STRAIGHT_ROAD_E2E,
+	STRAIGHT_ROAD_W2W,
 
-    /*
-     * corner 1
-     * +-----
-     * |a
-     * | b
-     * |  +--
-     * |  |
-     */
-    CORNER_ROAD_W2S, // a West then South
-    CORNER_ROAD_N2E, // b
-    /*
-     * corner 2
-     * ----+
-     *    b|
-     *   a |
-     * -+  |
-     *  |  |
-     */
-    CORNER_ROAD_E2S, // a
-    CORNER_ROAD_N2W, // b
-    /*
-     * corner 3
-     *  |  |
-     * -+  |
-     *   a |
-     *    b|
-     * ----+
-     */
-    CORNER_ROAD_S2W, // a
-    CORNER_ROAD_E2N, // b
-    /*
-     * corner 4
-     * |  |
-     * |  +-
-     * | b
-     * |a
-     * +----
-     */
-    CORNER_ROAD_S2E, // a
-    CORNER_ROAD_W2N, // b
+	/* corner 1 +----- |a | b | +-- | | */
+	CORNER_ROAD_W2S, // a West then South
+	CORNER_ROAD_N2E, // b
+	/* corner 2 ----+ b| a | -+ | | | */
+	CORNER_ROAD_E2S, // a
+	CORNER_ROAD_N2W, // b
+	/* corner 3 | | -+ | a | b| ----+ */
+	CORNER_ROAD_S2W, // a
+	CORNER_ROAD_E2N, // b
+	/* corner 4 | | | +- | b |a +---- */
+	CORNER_ROAD_S2E, // a
+	CORNER_ROAD_W2N, // b
 
-    /*
-     * Full Intersection
-     *   |  |
-     * --+  +--
-     *    ab
-     *    cd
-     * --+  +--
-     *   |  |
-     */
-    INTER_TURN_N2EN, // d links to North and East
-    INTER_TURN_W2NW, // b
-    INTER_TURN_S2WS, // a
-    INTER_TURN_E2SE, // c
-    /*
-     * Sample Partial Intersection - Note this is doable since all nodes only
-     * point forward. If backwards have more cases
-     * ----------
-     *     ab
-     *     cd
-     * ---+  +---
-     *    |  |
-     * b = STRAIGHT_ROAD_W
-     * a = INTER_TURN_S2WS // special case since no in path, but doesn't matter
-     * c = INTER_TURN_E2SE
-     * d = INTER_TURN_N2EN
-     */
+	/* Full Intersection | | --+ +-- ab cd --+ +-- | | */
+	INTER_TURN_N2EN, // d links to North and East
+	INTER_TURN_W2NW, // b
+	INTER_TURN_S2WS, // a
+	INTER_TURN_E2SE, // c
+	/* Sample Partial Intersection - Note this is doable since all nodes only
+	 * point forward. If backwards have more cases ---------- ab cd ---+ +--- |
+	 * | b = STRAIGHT_ROAD_W a = INTER_TURN_S2WS // special case since no in
+	 * path, but doesn't matter c = INTER_TURN_E2SE d = INTER_TURN_N2EN */
 
-    NON_ROAD
+	NON_ROAD
 }

commit 8363be647b926fd73948d2e101c1eda80553873c

Fix compilation error introduced in the 534c297 and 7eaada5
* Invalid package name
* Restore removed class

VERILOG_TOWN/verilogTown/src/com/me/myverilogTown/CarAnimateStates.java
@@ -1,0 +1,8 @@
+package com.me.myverilogTown;
+
+public enum CarAnimateStates 
+{
+	STOPPED,
+	MOVING
+}
+

VerilogEditor/src/VerilogEditor.java
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
  */
 
-package src;
-
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;