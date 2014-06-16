86f85dab06f47741b1a9fd4e0608f6ae0318133b
Naoki Mizuno <nm2372@gmail.com>
2014-06-16 17:19:22 -0400

    Remove unused directory ASSET_RESOURCES
    ASSET_RESOURCES in the Android project is used.

--- ASSET_RESOURCES/TILED_MAPS/base_gta_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="goodgtatileset9oz" tilewidth="64" tileheight="64" spacing="1">
- <image source="../goodgtatileset9oz.gif" width="600" height="600"/>
-</tileset>
