NigoroJr/verilogTown

commit 88a97bb4e3f9bb73c795f960dabfbf7867b82cea

Fix bug where incorrect type is exported for three-way intersections
This is caused by incorrect match of regex.

VERILOG_TOWN/LevelEditor/src/MapGrid.java
@@ -560,7 +560,7 @@ public class MapGrid extends JPanel implements MouseListener
 			{
 				Element intersection = doc.createElement("intersection");
 				// e.g. Change "FOUR_WAY_2" to "FOUR_WAY"
-				String intersectionType = this.type.replaceFirst("(?<=(FOUR|THREE)_WAY).*", "");
+				String intersectionType = this.type.replaceFirst("(?<=(FOUR_WAY|THREE_WAY_...).*", "");
 				intersection.appendChild(doc.createTextNode(intersectionType));
 
 				grid.appendChild(intersection);

commit aca3624fe91d7c3e30d8a7f0789c0b14ce8937c6

Fix bug where matching round bracket is missing in regex
This bug is introduced in 88a97bb.

VERILOG_TOWN/LevelEditor/src/MapGrid.java
@@ -560,7 +560,7 @@ public class MapGrid extends JPanel implements MouseListener
 			{
 				Element intersection = doc.createElement("intersection");
 				// e.g. Change "FOUR_WAY_2" to "FOUR_WAY"
-				String intersectionType = this.type.replaceFirst("(?<=(FOUR_WAY|THREE_WAY_...).*", "");
+				String intersectionType = this.type.replaceFirst("(?<=(FOUR_WAY|THREE_WAY_...)).*", "");
 				intersection.appendChild(doc.createTextNode(intersectionType));
 
 				grid.appendChild(intersection);